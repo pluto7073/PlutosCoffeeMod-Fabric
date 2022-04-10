67cdf79eaafc57037fe1975aa294a9cfcaeeacb5
pluto7073 <20planetpluto05@gmail.com>
2022-04-10 18:06:19 -0400

    Scraped the Coffee Machine Idea

--- src/main/java/ml/pluto7073/plutoscoffee/registry/ModStats.java
+++ src/main/java/ml/pluto7073/plutoscoffee/registry/ModStats.java
@@ -8,12 +8,9 @@ import net.minecraft.util.registry.Registry;
 
 public class ModStats {
 
-    public static final Identifier INTERACT_WITH_COFFEE_MACHINE = new Identifier(PlutosCoffee.MOD_ID, "interact_with_coffee_machine");
     public static final Identifier DRINK_COFFEE = new Identifier(PlutosCoffee.MOD_ID, "drink_coffee");
 
     public static void registerStats() {
-        Registry.register(Registry.CUSTOM_STAT, INTERACT_WITH_COFFEE_MACHINE.getPath(), INTERACT_WITH_COFFEE_MACHINE);
-        Stats.CUSTOM.getOrCreateStat(INTERACT_WITH_COFFEE_MACHINE, StatFormatter.DEFAULT);
         Registry.register(Registry.CUSTOM_STAT, DRINK_COFFEE.getPath(), DRINK_COFFEE);
         Stats.CUSTOM.getOrCreateStat(DRINK_COFFEE, StatFormatter.DEFAULT);
     }
