d00dd0c29a61dc67b9b67b12db2191537244d6c7
pluto7073 <20planetpluto05@gmail.com>
2023-02-01 20:10:57 -0500

    Added a ton of new Blocks and Items

--- src/main/java/ml/pluto7073/plutoscoffee/registry/ModBlocks.java
+++ src/main/java/ml/pluto7073/plutoscoffee/registry/ModBlocks.java
@@ -1,23 +1,43 @@
 package ml.pluto7073.plutoscoffee.registry;
 
+import com.mojang.datafixers.types.Type;
 import ml.pluto7073.plutoscoffee.PlutosCoffee;
+import ml.pluto7073.plutoscoffee.blocks.CoffeeBrewerBlock;
+import ml.pluto7073.plutoscoffee.blocks.CoffeeBrewerBlockEntity;
 import ml.pluto7073.plutoscoffee.blocks.CoffeeCrop;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import ml.pluto7073.plutoscoffee.blocks.CoffeeWorkstationBlock;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.datafixer.TypeReferences;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
-    public static final Block COFFEE_CROP = new CoffeeCrop();
+    public static Block COFFEE_CROP;
+    public static Block COFFEE_WORKSTATION;
+    public static Block COFFEE_BREWER;
 
-    static {
+    public static BlockEntityType<CoffeeBrewerBlockEntity> COFFEE_BREWER_BLOCK_ENTITY_TYPE;
 
+    private static Block register(String id, Block block) {
+        return Registry.register(Registry.BLOCK, new Identifier(PlutosCoffee.MOD_ID, id), block);
     }
 
-    public static void registerBlocks() {
-        Registry.register(Registry.BLOCK, new Identifier(PlutosCoffee.MOD_ID, "coffee_plant"), COFFEE_CROP);
+    private static <T extends BlockEntity> BlockEntityType<T> create(String id, BlockEntityType.Builder<T> builder) {
+        Type<?> type = Util.getChoiceType(TypeReferences.BLOCK_ENTITY, id);
+        return Registry.register(Registry.BLOCK_ENTITY_TYPE, id, builder.build(type));
+    }
+
+    public static void init() {
+        COFFEE_CROP = register("coffee_plant", new CoffeeCrop(AbstractBlock.Settings.of(Material.PLANT).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP).nonOpaque()));
+        COFFEE_BREWER = register("coffee_brewer", new CoffeeBrewerBlock(AbstractBlock.Settings.of(Material.METAL).requiresTool().strength(0.5F).luminance((state) -> 1).nonOpaque()));
+        COFFEE_WORKSTATION = register("coffee_workstation", new CoffeeWorkstationBlock(AbstractBlock.Settings.of(Material.WOOD).strength(2.5F).sounds(BlockSoundGroup.WOOD)));
+        COFFEE_BREWER_BLOCK_ENTITY_TYPE = create("coffee_brewer", BlockEntityType.Builder.create(CoffeeBrewerBlockEntity::new, COFFEE_BREWER));
     }
 }
--- src/main/java/ml/pluto7073/plutoscoffee/registry/ModItems.java
+++ src/main/java/ml/pluto7073/plutoscoffee/registry/ModItems.java
@@ -2,32 +2,60 @@ package ml.pluto7073.plutoscoffee.registry;
 
 import ml.pluto7073.plutoscoffee.PlutosCoffee;
 import ml.pluto7073.plutoscoffee.items.*;
-import net.minecraft.item.AliasedBlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.block.Block;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    public static final Item COFFEE_BEAN = new CoffeeBean();
-    public static final Item COFFEE_BERRY = new AliasedBlockItem(ModBlocks.COFFEE_CROP, new Item.Settings().group(ItemGroup.FOOD));
-    public static final Item GROUND_COFFEE = new GroundCoffee();
-    public static final Item COFFEE_BEAN_ROASTED = new CoffeeBeanRoasted();
-    public static final Item CHOCOLATE_COFFEE_BEAN = new ChocolateCoffeeBean();
-    public static final Item CHOCOLATE_GROUND_COFFEE = new ChocolateGroundCoffee();
-    public static final Item CARAMEL = new Caramel();
-    public static final Item MILK_BOTTLE = new MilkBottle();
+    public static Item COFFEE_BERRY;
+    public static Item COFFEE_WORKSTATION;
+    public static Item COFFEE_BREWER;
+    public static Item COFFEE_BEAN;
+    public static Item LIGHT_ROAST_BEAN;
+    public static Item MEDIUM_ROAST_BEAN;
+    public static Item DARK_ROAST_BEAN;
+    @Deprecated
+    public static Item MOCHA_BEAN;
+    public static Item GROUND_LIGHT_ROAST;
+    @Deprecated
+    public static Item GROUND_MOCHA_BEAN;
+    public static Item GROUND_MEDIUM_ROAST;
+    public static Item GROUND_DARK_ROAST;
+    public static Item GROUND_ESPRESSO_ROAST;
+    public static Item CARAMEL;
+    public static Item MOCHA_SAUCE;
+    public static Item MILK_BOTTLE;
 
-    public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier(PlutosCoffee.MOD_ID, "ground_coffee"), GROUND_COFFEE);
-        Registry.register(Registry.ITEM, new Identifier(PlutosCoffee.MOD_ID, "coffee_berry"), COFFEE_BERRY);
-        Registry.register(Registry.ITEM, new Identifier(PlutosCoffee.MOD_ID, "coffee_bean"), COFFEE_BEAN);
-        Registry.register(Registry.ITEM, new Identifier(PlutosCoffee.MOD_ID, "roasted_coffee_bean"), COFFEE_BEAN_ROASTED);
-        Registry.register(Registry.ITEM, new Identifier(PlutosCoffee.MOD_ID, "chocolate_coffee_bean"), CHOCOLATE_COFFEE_BEAN);
-        Registry.register(Registry.ITEM, new Identifier(PlutosCoffee.MOD_ID, "chocolate_ground_coffee"), CHOCOLATE_GROUND_COFFEE);
-        Registry.register(Registry.ITEM, new Identifier(PlutosCoffee.MOD_ID, "caramel"), CARAMEL);
-        Registry.register(Registry.ITEM, new Identifier(PlutosCoffee.MOD_ID, "milk_bottle"), MILK_BOTTLE);
+    public static Item BREWED_COFFEE;
+
+    private static Item register(String id, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(PlutosCoffee.MOD_ID, id), item);
+    }
+
+    private static Item register(Block block) {
+        return Registry.register(Registry.ITEM, Registry.BLOCK.getId(block), new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void init() {
+        COFFEE_BEAN = register("coffee_bean", new CoffeeBean());
+        COFFEE_BERRY = register("coffee_berry", new AliasedBlockItem(ModBlocks.COFFEE_CROP, new Item.Settings()));
+        GROUND_LIGHT_ROAST = register("ground_coffee", new Item(new Item.Settings()));
+        LIGHT_ROAST_BEAN = register("roasted_coffee_bean", new Item(new Item.Settings().food(CoffeeBean.COFFEE_BEAN_FOOD_COMPONENT.build())));
+        MOCHA_BEAN = register("chocolate_coffee_bean", new Item(new Item.Settings().food(CoffeeBean.COFFEE_BEAN_FOOD_COMPONENT.hunger(3).saturationModifier(2.5f).build())));
+        GROUND_MOCHA_BEAN = register("chocolate_ground_coffee", new Item(new Item.Settings()));
+        CARAMEL = register("caramel", new Item(new Item.Settings().recipeRemainder(Items.GLASS_BOTTLE).maxCount(16)));
+        MOCHA_SAUCE = register("mocha_syrup", new Item(new Item.Settings().recipeRemainder(Items.GLASS_BOTTLE).maxCount(16)));
+        MILK_BOTTLE = register("milk_bottle", new MilkBottle());
+        MEDIUM_ROAST_BEAN = register("medium_roast_bean", new Item(new Item.Settings().food(CoffeeBean.COFFEE_BEAN_FOOD_COMPONENT.build())));
+        GROUND_MEDIUM_ROAST = register("ground_medium_roast", new Item(new Item.Settings()));
+        DARK_ROAST_BEAN = register("dark_roast_bean", new Item(new Item.Settings().food(CoffeeBean.COFFEE_BEAN_FOOD_COMPONENT.build())));
+        GROUND_DARK_ROAST = register("ground_dark_roast", new Item(new Item.Settings()));
+        GROUND_ESPRESSO_ROAST = register("espresso_grounds", new Item(new Item.Settings()));
+        BREWED_COFFEE = register("brewed_coffee", new BrewedCoffee(new Item.Settings().maxCount(1)));
+        COFFEE_WORKSTATION = register(ModBlocks.COFFEE_WORKSTATION);
+        COFFEE_BREWER = register(ModBlocks.COFFEE_BREWER);
     }
 
 }
