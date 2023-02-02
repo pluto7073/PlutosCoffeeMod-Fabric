23f321bbbb204ef4d702a7ee6440e6ee4ec72a4e
pluto7073 <20planetpluto05@gmail.com>
2023-02-01 20:10:06 -0500

    Colour changer for the BrewedCoffee Item

---
+++ src/main/java/ml/pluto7073/plutoscoffee/mixins/client/ItemColorsMixin.java
@@ -0,0 +1,25 @@
+package ml.pluto7073.plutoscoffee.mixins.client;
+
+import ml.pluto7073.plutoscoffee.Utils;
+import ml.pluto7073.plutoscoffee.registry.ModItems;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.color.item.ItemColors;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemColors.class)
+@Environment(EnvType.CLIENT)
+public class ItemColorsMixin {
+
+    @Inject(at = @At("TAIL"), method = "create", cancellable = true)
+    private static void createInject(BlockColors blockColors, CallbackInfoReturnable<ItemColors> cir) {
+        ItemColors itemColors = cir.getReturnValue();
+        itemColors.register((stack, tintIndex) -> tintIndex > 0 ? -1 : Utils.getCoffeeColour(stack), ModItems.BREWED_COFFEE);
+        cir.setReturnValue(itemColors);
+    }
+
+}
