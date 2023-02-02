39910ff06fbf4a1d9dc66bb686861b5b663a8f66
pluto7073 <20planetpluto05@gmail.com>
2023-02-01 19:53:28 -0500

    Added a Coffee Brewer Block

--- src/main/java/ml/pluto7073/plutoscoffee/Client.java
+++ src/main/java/ml/pluto7073/plutoscoffee/Client.java
@@ -1,10 +1,14 @@
 package ml.pluto7073.plutoscoffee;
 
+import ml.pluto7073.plutoscoffee.gui.CoffeeBrewerScreen;
+import ml.pluto7073.plutoscoffee.gui.CoffeeWorkstationScreen;
 import ml.pluto7073.plutoscoffee.registry.ModBlocks;
+import ml.pluto7073.plutoscoffee.registry.ModScreens;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
@@ -12,6 +16,7 @@ public class Client implements ClientModInitializer {
 
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.COFFEE_CROP, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.COFFEE_BREWER, RenderLayer.getCutout());
     }
 
 }
