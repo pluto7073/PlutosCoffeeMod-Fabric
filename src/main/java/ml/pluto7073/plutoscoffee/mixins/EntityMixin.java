de6ee2eb50c7620a5d8881acabd5ba78e35f1cc6
pluto7073 <20planetpluto05@gmail.com>
2024-01-09 21:55:07 -0500

    Made cows milkable with bottles

--- src/main/java/ml/pluto7073/plutoscoffee/mixins/EntityMixin.java
+++ src/main/java/ml/pluto7073/plutoscoffee/mixins/EntityMixin.java
@@ -2,11 +2,14 @@ package ml.pluto7073.plutoscoffee.mixins;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.data.DataTracker;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Entity.class)
-public class EntityMixin {
+public abstract class EntityMixin {
+    @Shadow public abstract World getWorld();
+
     @Shadow @Final protected DataTracker dataTracker;
 }
