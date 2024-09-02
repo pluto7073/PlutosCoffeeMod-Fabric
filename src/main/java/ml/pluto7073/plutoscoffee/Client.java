d7922d3de1f539a50ed5b6767ec0ec3eb34941ee
pluto7073 <20planetpluto05@gmail.com>
2024-09-02 16:46:19 -0600

    Moved MenuScreen registration to FabricApi's

--- src/main/java/ml/pluto7073/plutoscoffee/Client.java
+++ src/main/java/ml/pluto7073/plutoscoffee/Client.java
@@ -1,9 +1,13 @@
 package ml.pluto7073.plutoscoffee;
 
 import ml.pluto7073.plutoscoffee.config.CoffeeConfig;
+import ml.pluto7073.plutoscoffee.gui.CoffeeBrewerScreen;
+import ml.pluto7073.plutoscoffee.gui.CoffeeGrindrScreen;
+import ml.pluto7073.plutoscoffee.gui.EspressoMachineScreen;
 import ml.pluto7073.plutoscoffee.network.ModPacketsS2C;
 import ml.pluto7073.plutoscoffee.registry.ModBlocks;
 import ml.pluto7073.plutoscoffee.registry.ModItems;
+import ml.pluto7073.plutoscoffee.registry.ModScreens;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -12,6 +16,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -28,6 +33,10 @@ public class Client implements ClientModInitializer {
                 ResourceManagerHelper.registerBuiltinResourcePack(new ResourceLocation(PlutosCoffee.MOD_ID, "dark_gui"), container, Component.translatable("pack.plutoscoffee.dark_gui"),ResourcePackActivationType.NORMAL));
 
         ModPacketsS2C.register();
+
+        MenuScreens.register(ModScreens.BREWER_MENU_TYPE, CoffeeBrewerScreen::new);
+        MenuScreens.register(ModScreens.GRINDR_SCREEN_HANDLER_TYPE, CoffeeGrindrScreen::new);
+        MenuScreens.register(ModScreens.ESPRESSO_SCREEN_HANDLER_TYPE, EspressoMachineScreen::new);
 
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.COFFEE_CROP, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.COFFEE_BREWER, RenderType.cutout());
